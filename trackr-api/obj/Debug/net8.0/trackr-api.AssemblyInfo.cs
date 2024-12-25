4df5e2862d1b3c755172f21f712328fa49de833a
teckhong <lthong96@gmail.com>
2024-12-26 03:54:43 +0800

    assembly issue  ?

--- trackr-api/obj/Debug/net8.0/trackr-api.AssemblyInfo.cs
+++ trackr-api/obj/Debug/net8.0/trackr-api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("trackr-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a241185b00cdca6df8e0f8e6714b183b6feb5a8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4973c9a724d164341de2fe91a92a4add5ff94f3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("trackr-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("trackr-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
